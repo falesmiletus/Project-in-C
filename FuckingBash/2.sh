24d5cb9714a8f244de8eb0152b111d01bb0540f7
falesmiletus <mixail.agafonov.1986@gmail.com>
2025-02-09 19:55:57 +0300

    Bash add

---
+++ FuckingBash/2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -n "Как вас зовут? "
+read Name
+echo "Привет $Name"
+echo " $(($1 $2 $3))"
---
+++ FuckingBash/FirstScripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+name="Артем"
+x=2
+y=3
+echo "Привет $name"
+new=$(($x+$y))
+echo $new
